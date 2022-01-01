9ea6ea551e2992c0707a5cc4b4e268967c25dac8
Leonardo Bollazzi <leonardo.bollazzi@students.fhnw.ch>
2022-01-01 15:34:08 +0100

    profile and search all

---
+++ src/main/java/ch/fhnw/acrm/controller/SearchAllController.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020. University of Applied Sciences and Arts Northwestern Switzerland FHNW.
+ * All rights reserved.
+ */
+
+package ch.fhnw.acrm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/searchAll")
+public class SearchAllController {
+
+    @GetMapping
+    public String getProfileView(){
+        return "acrm/searchAll.html";
+    }
+
+    @GetMapping("/create")
+    public String getCustomerCreateView(){
+        return "../acrm/customerCreate.html";
+    }
+
+    @GetMapping("/edit")
+    public String getCustomerEditView(){
+        return "../acrm/customerEdit.html";
+    }
+}
